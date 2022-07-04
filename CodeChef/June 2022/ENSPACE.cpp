1f2ad02af489a93692c075016f41c1d8fbabf5bf
Aadarshg087 <aadarshg087@gmail.com>
2022-07-04 12:55:30 +0530

    EXSPACE Code

---
+++ CodeChef/June 2022/ENSPACE.cpp
@@ -0,0 +1,30 @@
+/* Chef's computer has NN GB of free space. He wants to save XX files, each of size 1 GB and YY files, each of size 2 GB on his computer. Will he be able to do so?
+
+Chef can save all the files on his computer only if the total size of the files is less than or equal to the space available on his computer. */
+
+#include<iostream>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        int n;
+        cin>>n;
+        int x, y;
+        cin>>x>>y;
+        int X=x*1;
+        int Y=y*2;
+        if (X+Y<=n)
+        {
+            cout<<"YES"<<endl;
+        }
+        else
+            cout<<"NO"<<endl;
+        
+    }
+    
+    
+    return 0;
+}
