137ad1d35481ab1f431424d6e561b83164ad3cee
Aadarshg087 <aadarshg087@gmail.com>
2022-07-06 09:11:08 +0530

    PracList Question

---
+++ CodeChef/June 2022/PRACLIST.cpp
@@ -0,0 +1,13 @@
+/* Our Chef is currently practicing on CodeChef and is a beginner. The count of ‘All Problems’ in the Beginner section is XX. Our Chef has already ‘Attempted’ YY problems among them. How many problems are yet ‘Un-attempted’?  */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x, y;
+    cin >> x >> y;
+    cout << (x - y) << endl;
+
+    return 0;
+}
