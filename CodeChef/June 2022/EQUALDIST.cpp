f1a1b091dc8331ccaf562a5be02b4d99760cfc91
Aadarshg087 <aadarshg087@gmail.com>
2022-07-11 11:39:50 +0530

    Equal Question

---
+++ CodeChef/June 2022/EQUALDIST.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        int Choco_Alice, Choco_Bob;
+        cin>>Choco_Alice>>Choco_Bob;
+        if (Choco_Alice!=Choco_Bob)
+        {
+            for (int i = 0; i < max(Choco_Alice,Choco_Bob); i++)
+            {
+                
+            }
+            
+        }
+        
+    }
+    
+    return 0;
+}
