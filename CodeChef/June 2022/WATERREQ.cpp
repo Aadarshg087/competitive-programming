d62c0458140c2ae27301a3a198fd5ca288935ff3
Aadarshg087 <aadarshg087@gmail.com>
2022-07-12 13:18:57 +0530

    Water Question

---
+++ CodeChef/June 2022/WATERREQ.cpp
@@ -0,0 +1,17 @@
+/* Finally, after purchasing a water cooler during the April long challenge, Chef noticed that his water cooler requires 22 liters of water to cool for one hour.
+
+How much water (in liters) would be required by the cooler to cool for NN hours? */
+#include<iostream>
+using namespace std;
+
+int main(){
+    int t;
+    cin>>t;
+    while (t--)
+    {
+        int n;
+        cin>>n;
+        cout<<n*2<<endl;
+    }
+    return 0;
+}
