0126194e6defce0cdc3140c7b8bfbdfd8947f9c8
Aadarshg087 <aadarshg087@gmail.com>
2022-07-13 02:43:35 +0530

    Question Update

--- CodeChef/June 2022/BURGERS.cpp
+++ CodeChef/June 2022/BURGERS.cpp
@@ -1,6 +1,6 @@
 /* Chef is fond of burgers and decided to make as many burgers as possible.
 
-Chef has AA patties and BB buns. To make 11 burger, Chef needs 11 patty and 11 bun.
+Chef has A patties and BB buns. To make 11 burger, Chef needs 11 patty and 11 bun.
 Find the maximum number of burgers that Chef can make. */
 
 #include <iostream>
