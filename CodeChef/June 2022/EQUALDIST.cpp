78ff576e2cb29080bdb1b61f06d6fc3250b5405f
Aadarshg087 <aadarshg087@gmail.com>
2022-07-21 23:50:41 +0530

    equal question

--- CodeChef/June 2022/EQUALDIST.cpp
+++ CodeChef/June 2022/EQUALDIST.cpp
@@ -1,23 +1,25 @@
-#include<iostream>
+/* Alice and Bob are very good friends and they always distribute all the eatables equally among themselves.
+
+Alice has AA chocolates and Bob has BB chocolates. Determine whether Alice and Bob can distribute all the chocolates equally among themselves. */
+
+#include <iostream>
 using namespace std;
 
-int main(){
+int main()
+{
     int t;
-    cin>>t;
-    while (t--)
+    cin >> t;
+    while (t-- > 0)
     {
         int Choco_Alice, Choco_Bob;
-        cin>>Choco_Alice>>Choco_Bob;
-        if (Choco_Alice!=Choco_Bob)
+        cin >> Choco_Alice >> Choco_Bob;
+        int totalChoco = Choco_Alice + Choco_Bob;
+        if (totalChoco % 2 == 0)
         {
-            for (int i = 0; i < max(Choco_Alice,Choco_Bob); i++)
-            {
-                
-            }
-            
+            cout << "Yes" << endl;
         }
-        
+        else
+            cout << "No" << endl;
     }
-    
     return 0;
 }
