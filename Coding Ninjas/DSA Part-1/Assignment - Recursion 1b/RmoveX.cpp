196fdacebc5e9f8dd6d5d6318ee891931a06e3b2
Aadarsh <99075949+Aadarshg087@users.noreply.github.com>
2023-01-26 23:31:29 +0530

    Daily Coding Ninjas

---
+++ Coding Ninjas/DSA Part-1/Assignment - Recursion 1b/RmoveX.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int len(char input[])
+{
+    int count = 0;
+    for (int i = 0; input[i] != '\0'; i++)
+    {
+        count++;
+    }
+    return count;
+}
+
+void helper(char input[])
+{
+    int location = 0;
+    for (int i = 1; input[i] != '\0'; i++)
+    {
+        input[location] = input[i];
+        location++;
+    }
+    input[location] = '\0';
+    cout << input << endl;
+}
+
+void removeX(char input[])
+{
+    int length = len(input);
+    if (length == 0)
+    {
+        return;
+    }
+    if (input[0] == 'x')
+    {
+        // input[0] = input[1];
+        helper(input);
+        return removeX(input);
+    }
+    return removeX(input + 1);
+}
+
+int main()
+{
+    char s[100];
+    cin >> s;
+    removeX(s);
+    cout << s;
+    return 0;
+}
