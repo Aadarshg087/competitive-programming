76be8bf891f5ed992ee95085b783c6e962ba4cf7
aadarsh <99075949+Aadarshg087@users.noreply.github.com>
2023-08-14 11:47:39 +0530

    Coding Ninjas Problem

--- Coding Ninjas/DSA Part - 2/Lec 13 - Binary Search Tree/isBST.cpp
+++ Coding Ninjas/DSA Part - 2/Lec 13 - Binary Search Tree/isBST.cpp
@@ -25,24 +25,85 @@ public:
     }
 };
 
-bool isBST(BinaryTreeNode<int> *root)
+int maxElement(BinaryTreeNode<int> *root)
 {
-    if (root->left == NULL && root->right == NULL)
+    if (root == NULL)
     {
-        return true;
-    }
-    else if (root == NULL)
-    {
-        return false;
+        return 0; //
     }
 
-    if (root->right->data < root->data || root->left->data > root->data)
-    {
-        return false;
-    }
-
-    bool ans = isBST(root->left) && isBST(root->right);
+    int ans = max(root->data, max(maxElement(root->left), maxElement(root->right)));
     return ans;
+}
+
+int minElement(BinaryTreeNode<int> *root)
+{
+    if (root == NULL)
+    {
+        return INT_MAX;
+    }
+
+    int ans = min(root->data, min(minElement(root->left), minElement(root->right)));
+    return ans;
+}
+
+// ----------- Approach 1 -> Time Complexity = O(n^2) ------------
+// bool isBST(BinaryTreeNode<int> *root)
+// {
+//     if (root == NULL)
+//     {
+//         return true;
+//     }
+
+//     int leftMax = maxElement(root->left);
+//     int rightMin = minElement(root->right);
+
+//     if (root->data < leftMax || root->data > rightMin)
+//     {
+//         return false;
+//     }
+//     else
+//     {
+//         bool ans = isBST(root->left) && isBST(root->right);
+//         return ans;
+//     }
+
+//     // ---------- Another Approach --------------- //
+//     // bool output = (root->data > leftMax) && (root->data <= rightMin) && isBST(root->left) && isBST(root->right);
+//     // return output;
+// }
+
+// -------------------- Approach 2 Time complexity = O(n) -------------------------------
+class IsBSTReturn
+{
+public:
+    bool isBST;
+    int minimum;
+    int maximum;
+};
+
+IsBSTReturn isBST2(BinaryTreeNode<int> *root)
+{
+    if (root == NULL)
+    {
+        IsBSTReturn output;
+        output.isBST = true;
+        output.minimum = INT_MAX;
+        output.maximum = INT_MIN;
+        return output;
+    }
+
+    IsBSTReturn leftOutput = isBST2(root->left);
+    IsBSTReturn rightOutput = isBST2(root->right);
+    int maximum = max(root->data, max(leftOutput.maximum, rightOutput.maximum));
+    int minimum = min(root->data, min(leftOutput.minimum, rightOutput.minimum));
+    bool isBSTFinal = (root->data > leftOutput.maximum) && (root->data <= rightOutput.minimum) && leftOutput.isBST && rightOutput.isBST;
+
+    IsBSTReturn output;
+    output.isBST = isBSTFinal;
+    output.maximum = maximum;
+    output.minimum = minimum;
+    return output;
 }
 
 int main()
