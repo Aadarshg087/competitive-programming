0d3f138a5c6871d71414809eb6c5876d44570b85
aadarsh <99075949+Aadarshg087@users.noreply.github.com>
2023-08-15 23:56:44 +0530

    Coding Ninjas Problem

--- Coding Ninjas/DSA Part - 2/Lec 12 - Binary Trees/RemoveLeafNode.cpp
+++ Coding Ninjas/DSA Part - 2/Lec 12 - Binary Trees/RemoveLeafNode.cpp
@@ -22,22 +22,22 @@ BinaryTreeNode<int> *removeLeafNodes(BinaryTreeNode<int> *root)
 {
     if (root == NULL)
     {
-        return;
-    }
-
-    if (root->left == NULL && root->right == NULL)
-    {
         return NULL;
     }
 
-    BinaryTreeNode<int> *temp1 = removeLeafNodes(root->left);
-    BinaryTreeNode<int> *temp2 = removeLeafNodes(root->right);
-
-    if (temp1 == NULL && temp2 == NULL)
+    if (root->left->left == NULL && root->left->right == NULL)
     {
         root->left = NULL;
-        root->right = NULL;
+        return root;
     }
+    if (root->right->left == NULL && root->right->right == NULL)
+    {
+        root->right = NULL;
+        return root;
+    }
+
+    removeLeafNodes(root->left);
+    removeLeafNodes(root->right);
 
     return root;
 }
