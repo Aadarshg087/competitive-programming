0a2407950729a0d5ee03bfb7eea76311c7a7b101
aadarsh <99075949+Aadarshg087@users.noreply.github.com>
2023-08-17 15:44:16 +0530

    Construct Linked List from BSt

--- Coding Ninjas/DSA Part - 2/Lec 13 - Binary Search Tree/ConstructLLfromBST.cpp
+++ Coding Ninjas/DSA Part - 2/Lec 13 - Binary Search Tree/ConstructLLfromBST.cpp
@@ -50,6 +50,29 @@ int inorderTraversal(BinaryTreeNode<int> *root)
     inorderTraversal(root->right);
 }
 
+int MaxElement(BinaryTreeNode<int> *root)
+{
+    if (root == NULL)
+    {
+        return NULL;
+    }
+
+    int temp = max(root->data, max(MaxElement(root->left), MaxElement(root->right)));
+
+    return temp;
+}
+int MinElement(BinaryTreeNode<int> *root)
+{
+    if (root == NULL)
+    {
+        return NULL;
+    }
+
+    int temp = min(root->data, min(MinElement(root->left), MinElement(root->right)));
+
+    return temp;
+}
+
 Node<int> *constructLinkedList(BinaryTreeNode<int> *root)
 {
     if (root == NULL)
@@ -58,8 +81,67 @@ Node<int> *constructLinkedList(BinaryTreeNode<int> *root)
     }
 
     constructLinkedList(root->left);
-    Node<int> *newNode = new Node<int>(root->data);
     constructLinkedList(root->right);
+
+    int leftMax = MaxElement(root->left);
+    int rightMin = MinElement(root->right);
+
+    Node<int> *newNode = new Node<int>(root->data);
+    Node<int> *leftMaxNode = new Node<int>(leftMax);
+    Node<int> *rightMinNode = new Node<int>(rightMin);
+
+    leftMaxNode->next = newNode;
+    newNode->next = rightMinNode;
+}
+
+Node<int> *leftSubTree(BinaryTreeNode<int> *root)
+{
+    if (root == NULL)
+    {
+        return NULL;
+    }
+    if (root->left == NULL && root->right == NULL)
+    {
+        Node<int> *newNode = new Node<int>(root->data);
+        return newNode;
+    }
+
+    Node<int> *temp1 = leftSubTree(root->left);
+    Node<int> *temp2 = rightSubTree(root->right);
+    Node<int> *newNode = new Node<int>(root->data);
+
+    temp1->next = newNode;
+    newNode->next = temp2;
+}
+
+Node<int> *rightSubTree(BinaryTreeNode<int> *root)
+{
+    if (root == NULL)
+    {
+        return NULL;
+    }
+    if (root->left == NULL && root->right == NULL)
+    {
+        Node<int> *newNode = new Node<int>(root->data);
+        return newNode;
+    }
+
+    Node<int> *temp1 = leftSubTree(root->left);
+    Node<int> *temp2 = rightSubTree(root->right);
+}
+
+Node<int> *constructLinkedList(BinaryTreeNode<int> *root)
+{
+    if (root == NULL)
+    {
+        return NULL;
+    }
+    if (root->left == NULL && root->right == NULL)
+    {
+        Node<int> *newNode = new Node<int>(root->data);
+        return newNode;
+    }
+    pair<int *, int *> p;
 }
 
 int main()
