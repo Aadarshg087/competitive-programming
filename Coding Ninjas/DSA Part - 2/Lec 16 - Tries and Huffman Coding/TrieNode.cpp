0b9ed3132b8e9badce56e8f0fbf226467c9864d6
aadarsh <99075949+Aadarshg087@users.noreply.github.com>
2023-08-29 11:00:41 +0530

    TrieNode Introduction

---
+++ Coding Ninjas/DSA Part - 2/Lec 16 - Tries and Huffman Coding/TrieNode.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+class Trie
+{
+    TrieNode *root;
+
+public:
+    Trie()
+    {
+        root = new TrieNode('\0');
+    }
+
+    void insertWord(TrieNode *root, string word)
+    {
+        if (word.size() == 0)
+        {
+            root->isTerminal = true;
+        }
+
+        int index = word[0] - 'a';
+        TrieNode *child;
+        if (root->children[index] != NULL)
+        {
+        }
+    }
+    void insertWord(string word)
+    {
+        insertWord(root, word);
+    };
+};
+
+class TrieNode
+{
+public:
+    char data;
+    TrieNode **children;
+    bool isTerminal;
+    TrieNode(char data)
+    {
+        this->data = data;
+        children = new TrieNode *[26];
+        for (int i = 0; i < 26; i++)
+        {
+            children[i] = NULL;
+        }
+
+        isTerminal = false;
+    }
+};
+
+int main()
+{
+
+    return 0;
+}
