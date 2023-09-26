78b97067cb298c54535556db9f376411cadc1aef
aadarsh <99075949+Aadarshg087@users.noreply.github.com>
2023-09-26 10:57:11 +0530

    Linked List Practice

--- Practice/AppendLastNToFirst.cpp
+++ Practice/AppendLastNToFirst.cpp
@@ -21,16 +21,83 @@ public:
     }
 };
 
-void input()
+int count(Node *root)
 {
-    int data;
-    cin >> data;
-    Node *root = new Node(data);
+    Node *temp = root;
+    int count = 0;
+    while (temp != NULL)
+    {
+        count++;
+        temp = temp->next;
+    }
+    return count;
+}
+
+Node *input(Node *root, int data)
+{
+    Node *newNode = new Node(data);
+    if (root == NULL)
+    {
+        root = newNode;
+        return root;
+    }
+    Node *temp = root;
+    while (temp->next != NULL)
+    {
+        temp = temp->next;
+    }
+    temp->next = newNode;
+    return root;
+}
+
+void printLinkedList(Node *root)
+{
+    if (root == NULL)
+    {
+        cout << "List is empty" << endl;
+    }
+    Node *temp = root;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+
+// 1 2 3 4 5
+// 2
+
+Node *appendLastNtoFirst(Node *root, int x)
+{
+    Node *newRoot = root;
+    Node *temp = root;
+    int count = 1;
+    while (count < x) // 3
+    {
+        temp = temp->next;
+        if (count = x - 1)
+        {
+            
+        }
+        count++;
+    }
 }
 
 int main()
 {
+    int data;
+    cin >> data;
+    // Node *root = new Node(data);
     Node *root = NULL;
-    
+    while (data != -1)
+    {
+        root = input(root, data);
+        cin >> data;
+    }
+
+    // cout << "Count is: " << count(root) << endl;
+    printLinkedList(root);
+
     return 0;
 }
