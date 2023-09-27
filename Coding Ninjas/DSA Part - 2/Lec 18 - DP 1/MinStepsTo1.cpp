97e88576c795ed9bb333254bf82de179d6a84af6
aadarsh <99075949+Aadarshg087@users.noreply.github.com>
2023-09-27 06:05:11 +0530

    Min Steps to 1 Question

--- Coding Ninjas/DSA Part - 2/Lec 18 - DP 1/MinStepsTo1.cpp
+++ Coding Ninjas/DSA Part - 2/Lec 18 - DP 1/MinStepsTo1.cpp
@@ -2,28 +2,68 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int minSteps(int n)
+int minSteps2_helper(int n, int *ans)
 {
-    if (n == 1)
+    if (n <= 1)
     {
-        return 1;
+        return 0;
     }
 
-    int count3, count2, count;
+    if (ans[n] != -1)
+    {
+        return ans[n];
+    }
+
+    int count3 = INT_MAX, count2 = INT_MAX, count1 = INT_MAX;
+    if (n % 3 == 0)
+    {
+        count3 = minSteps2_helper(n / 3, ans);
+    }
+    else if (n % 2 == 0)
+    {
+        count2 = minSteps2_helper(n / 2, ans);
+    }
+    else
+        count1 = minSteps2_helper(n - 1, ans);
+
+    int min_ans = min(count3, min(count1, count2)) + 1;
+    ans[n] = min_ans;
+    return min_ans;
+}
+
+int minSteps2(int n)
+{
+    int *ans = new int[n + 1];
+    for (int i = 0; i < n + 1; i++)
+    {
+        ans[i] = -1;
+    }
+
+    ans[1] = 1;
+    return minSteps2_helper(n, ans);
+}
+
+int minSteps(int n)
+{
+    // Base Case
+    if (n <= 1)
+    {
+        return 0;
+    }
+
+    int count1 = INT_MAX, count2 = INT_MAX, count3 = INT_MAX;
     if (n % 3 == 0)
     {
         count3 = minSteps(n / 3);
     }
-    if (n % 2 == 0)
+    else if (n % 2 == 0)
     {
         count2 = minSteps(n / 2);
     }
-    count = minSteps(n - 1);
-
-    
-    // int min_count = min(count3, min(count2, count));
-
-    return count;
+    count1 = minSteps(n - 1);
+    cout << count1 << " " << count2 << " " << count3 << endl;
+    int ans = min(count3, min(count1, count2)) + 1;
+    return ans;
 }
 
 int main()
@@ -31,6 +71,7 @@ int main()
     int n;
     cin >> n;
 
-    cout << minSteps(n) << endl;
+    // cout << minSteps(n) << endl;
+    cout << minSteps2(n) << endl;
     return 0;
 }
