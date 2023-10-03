38785500e15932afff6b7f5a2c9b07356fbc966c
aadarsh <99075949+Aadarshg087@users.noreply.github.com>
2023-10-03 06:30:44 +0530

    revision

--- Coding Ninjas/DSA Part - 2/Lec 18 - DP 1/MinimumCount.cpp
+++ Coding Ninjas/DSA Part - 2/Lec 18 - DP 1/MinimumCount.cpp
@@ -2,71 +2,56 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int minCount2_helper(int n, int *arr)
-{
-    if (arr[n] != -1)
-    {
-        return arr[n];
-    }
-    
-
-    int ans = 1 + minCount2_helper(n - num, arr);
-}
-
-int minCount2(int n)
+int minCount1(int n)
 {
     int *arr = new int[n + 1];
-    for (int i = 0; i < n + 1; i++)
-    {
-        arr[i] = -1;
-    }
-    int arr[0] = 0;
-    int arr[1] = 1;
-    int arr[2] = 2;
-    int arr[3] = 3;
+    arr[0] = 0;
+    arr[1] = 1;
 
-    return minCount2(int n, arr);
+    for (int i = 1; i < n + 1; i++)
+    {
+        for (int j = 1; j * j <= i; j++)
+        {
+            arr[i] = min(arr[i], 1 + arr[i - j * j]);
+        }
+    }
+    return arr[n];
+}
+
+int minCount_helper(int n, int *arr)
+{
+    if (arr[n] != -1)
+        return arr[n];
+
+    int ans = n;
+    for (int i = 1; i * i <= n; i++)
+    {
+        ans = min(ans, 1 + minCount_helper(n - i * i, arr));
+    }
+
+    arr[n] = ans;
+    return ans;
 }
 
 int minCount(int n)
 {
-    // Base Case
-    if (n == 1 || n == 2 || n == 3)
-    {
-        return n;
-    }
-    else if (n == 0)
-    {
-        return 0;
-    }
+    int *arr = new int[n + 1];
 
-    // Small Calculations
-    int temp = 1;
-    int num;
-    int min = INT_MAX;
-    int ans;
-    while ((temp * temp) <= n)
+    for (int i = 1; i < n + 1; i++)
     {
-        num = temp;
-        num *= num;
-        ans = 1 + minCount(n - num);
-        if (ans < min)
-        {
-            min = ans;
-        }
-        temp++;
+        arr[i] = -1;
     }
+    arr[0] = 0;
+    arr[1] = 1;
 
-    // Recursive Call
-    return min;
-
-    // int ans = 1 + minCount(n);
-    // minCount_helper(temp, n);
+    return minCount_helper(n, arr);
 }
 
 int main()
 {
-    cout << minCount(115) << endl;
+    cout << minCount(9) << endl;
+
+    cout << minCount1(9) << endl;
 
     return 0;
 }
---
+++ Coding Ninjas/DSA Part - 2/Lec 18 - DP 1/tempCodeRunnerFile.cpp
@@ -0,0 +1,7 @@
+void printArray()
+    // {
+    //     for (int i = 0; i < n + 1; i++)
+    //     {
+    //         cout << arr[i] << " ";
+    //     }
+    // }
