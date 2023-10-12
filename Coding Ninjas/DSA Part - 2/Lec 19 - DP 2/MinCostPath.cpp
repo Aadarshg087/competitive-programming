b3ada5273488d39d5601ec2fc8f4095880d92e5f
aadarsh <99075949+Aadarshg087@users.noreply.github.com>
2023-10-12 06:53:07 +0530

    MinCostPath Question

--- Coding Ninjas/DSA Part - 2/Lec 19 - DP 2/MinCostPath.cpp
+++ Coding Ninjas/DSA Part - 2/Lec 19 - DP 2/MinCostPath.cpp
@@ -2,36 +2,84 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int n;
-int m;
-
-int minCostPath(int arr[][n], int m)
+// Memoization
+int minCostPath_helper2(int **input, int m, int n, int i, int j, int **output)
 {
-    // printing the function
-    for (int i = 0; i < n; i++)
+    // Base Case
+    if (i == m - 1 && j == n - 1)
     {
-        for (int j = 0; j < m; j++)
-        {
-            cout << arr[i][j] << " ";
-        }
-        cout << endl;
+        return input[i][j];
     }
+
+    if (i >= m || j >= n)
+    {
+        return INT_MAX;
+    }
+
+    // Checking whether the answer is there or not
+    if (output[i][j] != -1)
+    {
+        return output[i][j];
+    }
+
+    // Recursive Calls
+    int x = minCostPath_helper(input, m, n, i, j + 1);
+    int y = minCostPath_helper(input, m, n, i + 1, j + 1);
+    int z = minCostPath_helper(input, m, n, i + 1, j);
+
+    // Small calculation
+    int ans = min(x, min(y, z)) + input[i][j];
+
+    // Saving the ans for future use
+    output[i][j] = ans;
+
+    return ans;
+}
+
+// Brute Force Approach - O(3^n)
+int minCostPath_helper(int **input, int m, int n, int i, int j)
+{
+    // Base Case
+    if (i == m - 1 && j == n - 1)
+    {
+        return input[i][j];
+    }
+
+    if (i >= m || j >= n)
+    {
+        return INT_MAX;
+    }
+
+    // Recursive Calls
+    int x = minCostPath_helper(input, m, n, i, j + 1);
+    int y = minCostPath_helper(input, m, n, i + 1, j + 1);
+    int z = minCostPath_helper(input, m, n, i + 1, j);
+
+    // Small calculation
+    int ans = min(x, min(y, z)) + input[i][j];
+    return ans;
+}
+
+int minCostPath(int **input, int m, int n)
+{
+    minCostPath_helper(input, m, n, 0, 0);
 }
 
 int main()
 {
+    int n, m;
     cin >> n >> m;
     int arr[n][m];
 
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < m; j++)
-        {
-            cin >> arr[i][j];
-        }
-    }
+    // for (int i = 0; i < n; i++)
+    // {
+    //     for (int j = 0; j < m; j++)
+    //     {
+    //         cin >> arr[i][j];
+    //     }
+    // }
 
-    cout << minCostPath(arr, m) << endl;
+    // cout << minCostPath(arr, m) << endl;
 
     return 0;
 }
