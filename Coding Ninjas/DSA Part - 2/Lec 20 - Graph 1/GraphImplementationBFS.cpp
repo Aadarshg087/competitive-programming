0b21c8deb23e21d131e95a6700b5e4c2ffea5598
aadarsh <99075949+Aadarshg087@users.noreply.github.com>
2023-10-28 21:16:41 +0530

    Half graph implementation

--- Coding Ninjas/DSA Part - 2/Lec 20 - Graph 1/GraphImplementationBFS.cpp
+++ Coding Ninjas/DSA Part - 2/Lec 20 - Graph 1/GraphImplementationBFS.cpp
@@ -1,9 +1,54 @@
-#include<iostream>
+#include <iostream>
 #include <bits/stdc++.h>
 using namespace std;
 
+void print(int **edges, int v)
+{
+    for (int i = 0; i < v; i++)
+    {
+        for (int j = 0; j < v; j++)
+        {
+            cout << edges[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
 int main()
 {
-    
+    int v;
+    cin >> v;
+
+    int **edges = new int *[v];
+    for (int i = 0; i < v; i++)
+    {
+        edges[i] = new int[v];
+    }
+
+    for (int i = 0; i < v; i++)
+    {
+        for (int j = 0; j < v; j++)
+        {
+            edges[i][j] = 0;
+        }
+    }
+
+    print(edges, v);
+
+    cout << "Enter the number of edges: ";
+    int edge;
+    cin >> edge;
+    for (int i = 0; i < edge; i++)
+    {
+        int first, second;
+        cin >> first >> second;
+        edges[first][second] = 1;
+        edges[second][first] = 1;
+    }
+
+    print(edges, v);
+
+    int *visited = new int[v];
+
     return 0;
 }
