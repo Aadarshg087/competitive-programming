1cedd386688d7022034b2cc379d22251c01e9fb2
aadarsh <99075949+Aadarshg087@users.noreply.github.com>
2023-11-09 07:59:57 +0530

    Vectors Size vs Capacity

---
+++ Practice/vector.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    vector<int> v;
+    v.push_back(10);
+    v.push_back(10);
+    v.push_back(10);
+    v.push_back(10);
+    v.push_back(10);
+    v.push_back(10);
+    cout << v.size() << endl;
+    cout << v.capacity() << endl;
+    for (auto &i : v)
+        cout << i << " ";
+    cout << endl;
+    return 0;
+}
