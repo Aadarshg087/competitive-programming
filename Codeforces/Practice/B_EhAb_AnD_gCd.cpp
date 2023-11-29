2d20335d8b17cd2c66260f69bc9b960ba9f22145
aadarsh <99075949+Aadarshg087@users.noreply.github.com>
2023-11-29 22:23:25 +0530

    Number Theory Question

---
+++ Codeforces/Practice/B_EhAb_AnD_gCd.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+// Macros
+#define ll long long int
+
+void solve()
+{
+    ll x;
+    cin >> x;
+    cout << 1 << " " << x - 1 << endl;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    // solve();
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        solve();
+    }
+    return 0;
+}
