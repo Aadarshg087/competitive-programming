914e39b140a67255d362be21b3a768d48627403e
aadarsh <99075949+Aadarshg087@users.noreply.github.com>
2024-01-16 03:14:50 +0530

    Upsolve CF Round 920 DIv. 3

--- Codeforces/Codeforces Round 920 (Div. 3)/C.cpp
+++ Codeforces/Codeforces Round 920 (Div. 3)/C.cpp
@@ -15,58 +15,85 @@ void solve()
     {
         cin >> v[i];
     }
-    if (n == 1)
-    {
-        if (initialCharge > v[0])
-        {
-            cout << "YES" << endl;
-            return;
-        }
-        cout << "NO" << endl;
-        return;
-    }
 
-    ll diff = abs(0 - v[0]);
-    ll drop = diff * ChargePerUnit;
-    if (drop < OnOff)
+    ll sum = v[0] - 0;
+    for (int i = 0; i < n - 1; i++)
     {
-        initialCharge -= drop;
-        // cout << initialCharge << endl;
+        sum += v[i + 1] - v[i];
+    }
+    // cout << sum << endl;
+
+    if (sum < initialCharge)
+    {
+        cout << "YES" << endl;
     }
     else
     {
-        initialCharge -= OnOff;
-    }
-
-    if (initialCharge <= 0)
-    {
         cout << "NO" << endl;
-        return;
     }
-
-    for (int i = 0; i < n - 1; i++)
-    {
-        diff = abs(v[i] - v[i + 1]);
-
-        drop = diff * ChargePerUnit;
-        if (drop < OnOff)
-        {
-            initialCharge -= drop;
-            // cout << initialCharge << endl;
-        }
-        else
-        {
-            initialCharge -= OnOff;
-        }
-
-        if (initialCharge <= 0)
-        {
-            cout << "NO" << endl;
-            return;
-        }
-    }
-    cout << "YES" << endl;
 }
+
+// void solve()
+// {
+//     ll n, initialCharge, ChargePerUnit, OnOff;
+//     cin >> n >> initialCharge >> ChargePerUnit >> OnOff;
+//     vector<ll> v(n);
+//     for (int i = 0; i < n; i++)
+//     {
+//         cin >> v[i];
+//     }
+//     if (n == 1)
+//     {
+//         if (initialCharge > v[0])
+//         {
+//             cout << "YES" << endl;
+//             return;
+//         }
+//         cout << "NO" << endl;
+//         return;
+//     }
+
+//     ll diff = abs(0 - v[0]);
+//     ll drop = diff * ChargePerUnit;
+//     if (drop < OnOff)
+//     {
+//         initialCharge -= drop;
+//         // cout << initialCharge << endl;
+//     }
+//     else
+//     {
+//         initialCharge -= OnOff;
+//     }
+
+//     if (initialCharge <= 0)
+//     {
+//         cout << "NO" << endl;
+//         return;
+//     }
+
+//     for (int i = 0; i < n - 1; i++)
+//     {
+//         diff = abs(v[i] - v[i + 1]);
+
+//         drop = diff * ChargePerUnit;
+//         if (drop < OnOff)
+//         {
+//             initialCharge -= drop;
+//             // cout << initialCharge << endl;
+//         }
+//         else
+//         {
+//             initialCharge -= OnOff;
+//         }
+
+//         if (initialCharge <= 0)
+//         {
+//             cout << "NO" << endl;
+//             return;
+//         }
+//     }
+//     cout << "YES" << endl;
+// }
 
 int main()
 {
--- Codeforces/Codeforces Round 920 (Div. 3)/D.cpp
+++ Codeforces/Codeforces Round 920 (Div. 3)/D.cpp
@@ -21,71 +21,34 @@ void solve()
     {
         cin >> arr2[i];
     }
-    // sort(arr1.begin(), arr1.end(), greater<ll>());
-    // sort(arr2.begin(), arr2.end());
+    sort(arr1.begin(), arr1.end());
+    sort(arr2.begin(), arr2.end());
 
-    int ans = 0;
-    // int k = 0;
-    // int j = m - 1;
+    ll ans = 0;
 
-    for (int i = 0; i < n; i++)
+    int la = 0;
+    int ra = n - 1;
+    int lb = 0;
+    int rb = m - 1;
+    while (la <= ra)
     {
-        int maxDiff = 0;
-
-        for (int l = 0; l < m; l++)
+        int diff1 = abs(arr1[la] - arr2[rb]);
+        int diff2 = abs(arr1[ra] - arr2[lb]);
+        if (diff1 >= diff2)
         {
-            cout << " is called" << endl;
-            if (arr2[l] == -1)
-            {
-                continue;
-            }
-            int diff = abs(arr1[i] - arr2[l]);
-            if (diff > maxDiff)
-            {
-                arr2[l] = -1;
-                maxDiff = diff;
-            }
+            ans += diff1;
+            la++;
+            rb--;
         }
-        ans += maxDiff;
-        // int diff1 = abs(arr1[i] - arr2[k]);
-        // int diff2 = abs(arr1[i] - arr2[j]);
-        // if (diff1 > diff2)
-        // {
-        //     ans += diff1;
-        //     k++;
-        // }
-        // else
-        // {
-        //     ans += diff2;
-        //     j--;
-        // }
+        else
+        {
+            ans += diff2;
+            ra--;
+            lb++;
+        }
     }
 
     cout << ans << endl;
-
-    // for (auto i : arr2)
-    //     cout << i << " ";
-    // cout << endl;
-    // ll ans = 0;
-    // int j = m - 1;
-    // int k = 0;
-    // for (int i = 0; i < n; i++)
-    // {
-    //     // cout << arr1[i] << " " << arr2[i] << endl;
-    //     // cout << arr1[i] << " " << arr2[j] << endl;
-    //     int diff1 = abs(arr1[i] - arr2[k]);
-    //     int diff2 = abs(arr1[i] - arr2[j]);
-    //     if (diff2 > diff1)
-    //     {
-    //         ans += diff2;
-    //         j--;
-    //     }
-    //     else
-    //     {
-    //         ans += diff1;
-    //         k++;
-    //     }
-    // }
 
     // cout << ans << endl;
 }
