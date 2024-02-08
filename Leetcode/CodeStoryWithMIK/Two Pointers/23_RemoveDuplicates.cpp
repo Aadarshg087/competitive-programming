418fc404b9591d159266694d594bc192f9443b98
aadarsh <99075949+Aadarshg087@users.noreply.github.com>
2024-02-08 10:35:57 +0530

    Two Potiners Practice

--- Leetcode/CodeStoryWithMIK/Two Pointers/23_RemoveDuplicates.cpp
+++ Leetcode/CodeStoryWithMIK/Two Pointers/23_RemoveDuplicates.cpp
@@ -4,6 +4,18 @@ using namespace std;
 
 int removeDuplicates(vector<int> &nums)
 {
+    int j = 0;
+    int count = 1;
+    for (int i = 1; i < nums.size(); i++)
+    {
+        if (nums[i - 1] != nums[i])
+        {
+            nums[++j] = nums[i];
+            count++;
+        }
+    }
+
+    return count;
 }
 
 int main()
