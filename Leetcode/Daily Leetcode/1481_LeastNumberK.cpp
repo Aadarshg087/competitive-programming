0471237d705618c20205e495a62609dccb938544
aadarsh <99075949+Aadarshg087@users.noreply.github.com>
2024-02-16 09:02:07 +0530

    Daily Leetcode

---
+++ Leetcode/Daily Leetcode/1481_LeastNumberK.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+/*
+    - Check for overflow
+    - Add brackets while using bitwise
+    - Check corner cases (out of bounds for loops)
+    - Revise the code
+    - Try to prove yourself wrong
+*/
+int findLeastNumOfUniqueInts(vector<int> &arr, int k)
+{
+    map<int, int> mp;
+    int n = arr.size();
+    for (int i = 0; i < n; i++)
+    {
+        mp[arr[i]]++;
+    }
+
+    map<int, int> mp1;
+    for (auto i : mp)
+    {
+        mp1.insert({i.second, i.first});
+    }
+
+    int count = 0;
+    for (auto i : mp1)
+    {
+        if (k > i.second)
+            k -= i.second;
+
+        if (i.second > 0)
+            count++;
+    }
+    return count;
+}
+
+int main()
+{
+    vector<int> arr{};
+    cout << findLeastNumOfUniqueInts(arr) << endl;
+
+    return 0;
+}
