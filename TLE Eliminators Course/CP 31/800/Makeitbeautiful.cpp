2c5c19a7d1351d1f46c6c7e58629fdc3edc108e0
aadarsh <99075949+Aadarshg087@users.noreply.github.com>
2024-04-05 23:43:53 +0530

    Codeforces Practice

--- TLE Eliminators Course/CP 31/800/Makeitbeautiful.cpp
+++ TLE Eliminators Course/CP 31/800/Makeitbeautiful.cpp
@@ -31,34 +31,91 @@ void solve()
     int n;
     cin >> n;
     vector<int> arr(n);
+    // 1 2 3 4 5
+    // 0 1
+    int sum = 0;
+    bool c = 0;
     for (int i = 0; i < n; i++)
     {
         cin >> arr[i];
+        if (sum == arr[i])
+        {
+            c = 1;
+        }
+        sum += arr[i];
     }
 
-    bool c = beauti(arr, n);
-
-    int a = *min_element(arr.begin(), arr.end());
-    int b = *max_element(arr.begin(), arr.end());
-    if (a == b)
+    if (!c)
     {
-        cout << "NO" << endl;
+        cout << "YES" << endl;
+        for (int i : arr)
+            cout << i << " ";
+        cout << endl;
         return;
+    }
+
+    sort(arr.begin(), arr.end(), greater<int>());
+    sum = 0;
+    c = 0;
+    int i = 0;
+    int j = n - 1;
+    vector<int> v;
+    while (i <= j)
+    {
+        if (i == j)
+            v.push_back(arr[i++]);
+        else
+        {
+            v.push_back(arr[i++]);
+            v.push_back(arr[j--]);
+        }
+    }
+
+    for (int i = 0; i < n; i++)
+    {
+        // cout << v[i] << " ";
+        if (sum == v[i])
+        {
+            // cout << "Not fixed" << endl;
+            c = 1;
+        }
+        sum += v[i];
     }
 
     if (c)
     {
-        cout << "YES" << endl;
-        print(arr);
-        return;
+        cout << "NO" << endl;
     }
     else
     {
         cout << "YES" << endl;
-        sort(arr.begin(), arr.end());
-        reverse(arr.begin(), arr.end());
-        print(arr);
+        for (int i : v)
+            cout << i << " ";
+        cout << endl;
     }
+    // bool c = beauti(arr, n);
+
+    // int a = *min_element(arr.begin(), arr.end());
+    // int b = *max_element(arr.begin(), arr.end());
+    // if (a == b)
+    // {
+    //     cout << "NO" << endl;
+    //     return;
+    // }
+
+    // if (c)
+    // {
+    //     cout << "YES" << endl;
+    //     print(arr);
+    //     return;
+    // }
+    // else
+    // {
+    //     cout << "YES" << endl;
+    //     sort(arr.begin(), arr.end());
+    //     reverse(arr.begin(), arr.end());
+    //     print(arr);
+    // }
 }
 int main()
 {
